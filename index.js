ee9f2cf21618cf7b32691369fdc7d57dc8b46325
Stephen Wang <stephenwang1234567890@gmail.com>
2020-12-31 19:00:23 -0500

    deployment

--- index.js
+++ index.js
@@ -43,4 +43,12 @@ app.listen(port, ()=>{
     console.log("Server Started.");
 })
 
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      })
+}
+
 //module.exports = router;
